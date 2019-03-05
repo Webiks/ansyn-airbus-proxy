790ee9e4a08589465b89ef52f90fc70190b2e32b
Tzahi Levi <thetzahilevi@gmail.com>
2019-03-05 08:54:11 +0200

    add airbus find by id

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM node
 WORKDIR /airbusProxy
 
 COPY . .
-
+RUN npm install
 EXPOSE 8100
 
 CMD ["node" , "index"]
--- index.js
+++ index.js
@@ -1,13 +1,53 @@
 const express = require('express');
 const cors = require('cors');
 const request = require('request');
+const cheerio = require('cheerio');
 const app = express();
-const PORT =  8100;
+
+const PORT = 8100;
 const USER = process.env.AIRBUS_USER || '';
 const PASSWORD = process.env.AIRBUS_PASS || '';
-app.use(cors({origin: true, credentials: true}));
+app.use(cors({ origin: true, credentials: true }));
 
-app.get('/search' , (req, res) => {
+app.get('/', (req, res) => {
+    console.log('req.', req.query);
+    const options = {
+        uri: `https://tiles.airbusds-geo.com/basic/metadata/${req.query.id}`,
+        json: true,
+        auth: {
+            user: USER,
+            pass: PASSWORD,
+            sendImmediately: false
+        }
+    };
+    request(options, (err, resp, body) => {
+        const keys = ['UID', 'nBits','wmtsUrl','cloudCover','orientationAngle','centerCol','radiometricAdjustment',
+            'gsUri','constellation','viewingAngle','snowCover','dataFileFormat','centerRow','nCols','viewingAlongTrack','illuminationAzimuthAngle',
+            'gsdAlongTrack','nRows','instrument','gsdAcrossTrack','passDirection','centerLatitude','satellite','illuminationElevationAngle',
+            'acquisitionDate','viewingAcrossTrack','lastModifiedDate','provider','crsCode','tileEngineUrl', 'incidenceAngle', 'sourceId',
+            'incidenceAcrossTrack', 'spectralProcessing', 'processingLevel', 'centerLongitude', 'nBands', 'incidenceAlongTrack', 'resolution','Geometry'];
+        let $ =cheerio.load(body);
+        const overlay = {};
+        //{"type":"Polygon","orientation":"clockwise","coordinates":[[[1.135122685185185,43.77625694444444],[1.398576388888888,43.77625694444444],[1.398576388888888,43.38513657407407],[1.135122685185185,43.38513657407407],[1.135122685185185,43.77625694444444]]]}
+        $('dt').each( (i, el) => {
+            const key = $(el).text().trim();
+            console.log(`searching... ${key}`);
+            if(keys.includes(key)) {
+                const value = $(el).next().text().trim();
+                console.log(`key: ${key} , value: ${value}`);
+                overlay[key] = value;
+            }
+        });
+
+
+
+        res.json({id: overlay.UID, properties: overlay, geometry: {
+            type: 'Polygon', orientation: 'clockwise', coordinates: JSON.parse(overlay.Geometry)
+            }});
+    })
+});
+
+app.get('/search', (req, res) => {
     let query = '?';
     Object.entries(req.query).forEach(q => {
         console.log(q);
@@ -22,19 +62,18 @@ app.get('/search' , (req, res) => {
             sendImmediately: false
         }
     };
-    request(options, (err,response, body)=> {
-        if(err) res.status(response? response.statusCode : 500).send(err.message);
-        else{
+    request(options, (err, response, body) => {
+        if (err) res.status(response ? response.statusCode : 500).send(err.message);
+        else {
             res.send(body);
         }
     });
 
 });
 
-
-app.get('/xyz' , (req, res)=>{
-    let {original } = req.query;
-    original = original.replace('https://tiles.airbusds-geo.com/' , 'https://tiles.airbusds-geo.com/basic/');
+app.get('/xyz', (req, res) => {
+    let { original } = req.query;
+    original = original.replace('https://tiles.airbusds-geo.com/', 'https://tiles.airbusds-geo.com/basic/');
     console.log('request tile : ' + original);
     const opt = {
         uri: original,
@@ -50,4 +89,4 @@ app.get('/xyz' , (req, res)=>{
 });
 
 
-app.listen(PORT, () => console.log('listening ' , PORT));
+app.listen(PORT, () => console.log('listening ', PORT));
